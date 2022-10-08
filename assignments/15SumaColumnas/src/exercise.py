d00a4bd07f300af8e596d28f00670ab9df2fbdd4
A0174935 <110852002+A0174935@users.noreply.github.com>
2022-10-08 09:33:47 -0500

    Update exercise.py

--- assignments/15SumaColumnas/src/exercise.py
+++ assignments/15SumaColumnas/src/exercise.py
@@ -1,14 +1,42 @@
-def generaMatriz (renglon, columna):
-    pass
+def generaMatriz(renglon,columna):
+  matriz=[]
+  for reng in range(renglon):
+    lista=[]
+    for col in range(columna):
+      num=int(input())
+      lista.append(num)
+    matriz.append(lista)
+  return (matriz)
 
-def sumaColumnas (matriz):
-    pass
-
+def sumaColumnas(matriz,renglon,columna):
+  deflist=[]
+  renglon_suma=0
+  columna_suma=-1
+  b=0
+  c=0
+  z=0
+  for i in range(columna):
+    columna_suma+=1
+    z=0
+    c=0
+    b=0
+    renglon_suma=0
+    for n in range(renglon):
+      c=c+b
+      b=matriz[renglon_suma][columna_suma]
+      renglon_suma=renglon_suma+1
+    z=c+b
+    deflist.append(z)
+  return (deflist)
 
 def main():
-    pass
+  renglon=int(input())
+  columna=int(input())
+  if renglon<=0 or columna<=0:
+    print("Error")
+  else:
+    matriz=generaMatriz(renglon,columna)
+    print(sumaColumnas(matriz,renglon,columna))
 
-
-
-if __name__=='__main__':
+if _name_ == '_main_':
     main()
